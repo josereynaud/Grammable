d3bf6cfe6eab8ce00a77a7225fc97859fc9534e5
josereynaud <you@example.com>
2019-07-08 11:14:05 -0400

    CRUD tests

--- app/controllers/grams_controller.rb
+++ app/controllers/grams_controller.rb
@@ -1,11 +1,16 @@
 class GramsController < ApplicationController
-    before_action :authenticate_user!, only: [:new, :create]
+    before_action :authenticate_user!, only: [:new, :create, :edit, :update, :destroy]
     
     def new
         @gram = Gram.new
       end
 
       def index
+      end
+
+      def show
+        @gram = Gram.find_by_id(params[:id])
+        return render_not_found if @gram.blank?
       end
 
       def create
@@ -16,10 +21,40 @@ class GramsController < ApplicationController
             render :new, status: :unprocessable_entity
           end
       end
+
+      def edit
+        @gram = Gram.find_by_id(params[:id])
+        return render_not_found if @gram.blank?
+      end
+
+      def update
+        @gram = Gram.find_by_id(params[:id])
+        return render_not_found if @gram.blank?
+
+        @gram.update_attributes(gram_params)
+
+        if @gram.valid?
+            redirect_to root_path
+        else
+            return render :edit, status: :unprocessable_entity
+        end
+      end
+
+      def destroy
+        @gram = Gram.find_by_id(params[:id])
+        return render_not_found if @gram.blank?
+        expect(response).to redirect_to root_path
+        @gram.destroy
+        redirect_to root_path
+      end
       
       private
 
       def gram_params
         params.require(:gram).permit(:message)
+      end
+
+      def render_not_found
+        render plain: 'Not Found :(', status: :not_found
       end
 end
